a9fe21ceaefd2446e6fa82bc4d195e02a9fc50e6
aRidiculousBoy <a1272265541@gmail.com>
2022-10-23 17:37:08 +0800

    [update]项目基础结构搭建

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import '@assets/css/base.css'
 
-Vue.config.productionTip = false;
+// 关闭生产提示
+Vue.config.productionTip = false
+
+Vue.use(Antd)
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+
+const routes = [
+  {
+    path: '/',
+    name: 'Layout',
+    component: () => import('@/layout/index.vue')
+  }
+]
+
+export default routes
