0f58e8be823ee708977f598fc68f575fc4bed7b2
aRidiculousBoy <a1272265541@gmail.com>
2022-11-01 20:48:57 +0800

    [update]分析页部分图表开发完成

--- src/views/dashboard/analysis/index.vue
+++ src/views/dashboard/analysis/index.vue
@@ -1,10 +1,37 @@
 <template>
-  <div class="page-analysis">analysis</div>
+  <div class="page-analysis">
+    <a-row :gutter="16">
+      <a-col :span="6">
+        <total-sale></total-sale>
+      </a-col>
+      <a-col :span="6">
+        <access-count></access-count>
+      </a-col>
+      <a-col :span="6">
+        <pay-count></pay-count>
+      </a-col>
+      <a-col :span="6">
+        <op />
+      </a-col>
+    </a-row>
+  </div>
 </template>
-
 <script>
+import TotalSale from './components/totalSale'
+import AccessCount from './components/accessCount'
+import PayCount from './components/payCount'
+import Op from './components/op'
+
 export default {
-  name: 'Analysis'
+  name: 'Analysis',
+  mounted() {},
+  methods: {},
+  components: {
+    TotalSale,
+    AccessCount,
+    PayCount,
+    Op
+  }
 }
 </script>
 
